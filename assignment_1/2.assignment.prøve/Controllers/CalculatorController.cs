3d76e7388528327f088dc07c768304a206806f27
obada <obadaaldweiri@gmail.com>
2022-09-23 13:39:35 +0200

    Projects

---
+++ assignment_1/2.assignment.prøve/Controllers/CalculatorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace assignment_2.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ assignment_1/assignment_3/assignment_3/assignment_3/Views/Guestbook/Add.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace assignment_3.Views.Guestbook
+{
+    public class add : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
